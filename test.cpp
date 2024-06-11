023251f18a280f300cf8e7b0ad12c3572b59fcca
WildanMukmin <wildanmukmin26@gmail.com>
2024-06-11 19:58:41 +0700

    merge

--- test.cpp
+++ test.cpp
@@ -2,8 +2,10 @@
 
 
 int main(){
-    system("g++ main.cpp -lwinmm");
-    system("a");
+    // system("g++ main.cpp -lwinmm");
+    // system("a");
+
+    PlaySound(TEXT("music/transfer.wav"), NULL, SND_SYNC);
 
     return 0;
 }
